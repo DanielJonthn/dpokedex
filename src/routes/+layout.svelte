02188d41000d1dc7bf6d689b0ab26b1444436ed7
DanielJonthn <daniel.378.dj@gmail.com>
2025-03-26 18:11:06 +0700

    Feat: Implemented all core features

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import "/src/app.css";
+  import { fly } from "svelte/transition";
+  import { page } from "$app/stores";
+</script>
+
+{#key $page.url.pathname}
+  <div
+    in:fly={{ y: 5, duration: 250, delay: 300 }}
+    out:fly={{ y: -5, duration: 200 }}
+  >
+    <slot />
+  </div>
+{/key}
